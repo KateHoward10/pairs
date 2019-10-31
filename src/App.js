00e5898a43ef4055f4b559809d632a9c94a02d2f
Kate Howard <k.l.howard.1992@gmail.com>
2019-10-31 22:32:32 +0000

    Display items for one second

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useInterval from './hooks/useInterval';
 import './App.css';
 
 function App() {
   const items = ['🥕', '🍅', '🥔', '🥑', '🌽', '🥦', '🍄', '🍆'];
+  const [itemsVisible, toggleItemsVisible] = useState(false);
+
+  function startGame() {
+    toggleItemsVisible(true);
+  }
+
+  useInterval(
+    () => {
+      toggleItemsVisible(false);
+    },
+    itemsVisible ? 1000 : null
+  );
 
   return (
-    <div className="grid">
-      {[...items, ...items]
-        .sort(() => Math.random() - 0.5)
-        .map((item, index) => (
-          <button key={index} className="item">
-            {item}
-          </button>
-        ))}
-    </div>
+    <React.Fragment>
+      <button onClick={startGame}>Play</button>
+      <div className="grid">
+        {[...items, ...items]
+          .sort(() => Math.random() - 0.5)
+          .map((item, index) => (
+            <button key={index} value={item} className="item">
+              {itemsVisible ? item : ''}
+            </button>
+          ))}
+      </div>
+    </React.Fragment>
   );
 }
 
