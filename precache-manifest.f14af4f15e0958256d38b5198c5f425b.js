ca7126af8c3c435b78cfd79a6bb0428721b975d7
Kate Howard <k.l.howard.1992@gmail.com>
2019-11-30 23:30:08 +0000

    Updates

---
+++ precache-manifest.f14af4f15e0958256d38b5198c5f425b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba394f57097b7aa209993ff73de0859",
+    "url": "/pairs/index.html"
+  },
+  {
+    "revision": "c7735c5b2d9ab3b43ed0",
+    "url": "/pairs/static/css/main.e2a50b3a.chunk.css"
+  },
+  {
+    "revision": "3cca04493f9349c555ed",
+    "url": "/pairs/static/js/2.3c42c4a1.chunk.js"
+  },
+  {
+    "revision": "c7735c5b2d9ab3b43ed0",
+    "url": "/pairs/static/js/main.a6b74e05.chunk.js"
+  },
+  {
+    "revision": "837cd12979944f829cd5",
+    "url": "/pairs/static/js/runtime-main.87c24bc3.js"
+  },
+  {
+    "revision": "58e9d8a0327a917d6a2e6a03451a00dc",
+    "url": "/pairs/static/media/gb.58e9d8a0.jpeg"
+  },
+  {
+    "revision": "aa54a71dd5c8c5b55ad281f0be9d3fbe",
+    "url": "/pairs/static/media/gb.aa54a71d.mp3"
+  },
+  {
+    "revision": "559fcd80b138275244d930cf608bdffc",
+    "url": "/pairs/static/media/🌶️.559fcd80.mp3"
+  },
+  {
+    "revision": "77ad066fbe5ded1b83a10108b59e842f",
+    "url": "/pairs/static/media/🍄.77ad066f.mp3"
+  },
+  {
+    "revision": "079d207c93ce5aefe33c8e627f3b6f5d",
+    "url": "/pairs/static/media/🍅.079d207c.mp3"
+  },
+  {
+    "revision": "86b9e5fe2bce23c38f2e64b11fecfa89",
+    "url": "/pairs/static/media/🥔.86b9e5fe.mp3"
+  },
+  {
+    "revision": "3b210c46ca08b8cc66a58c8dc821a5e2",
+    "url": "/pairs/static/media/🥕.3b210c46.mp3"
+  },
+  {
+    "revision": "c0a30f4403c121e1eff1f8c72aca29c3",
+    "url": "/pairs/static/media/🥦.c0a30f44.mp3"
+  },
+  {
+    "revision": "9c24decd925c500b945dcc991db836d8",
+    "url": "/pairs/static/media/🥬.9c24decd.mp3"
+  }
+]);
